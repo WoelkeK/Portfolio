2b69bedad0d6fb5047cbefe4eb321026c0e3718d
Krzysztof <krzysiek@woelke.pl>
2023-03-26 13:11:56 +0200

    add progressbar classes

--- src/app/components/progressbar/progressbar.component.ts
+++ src/app/components/progressbar/progressbar.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,Attribute, Input} from '@angular/core';
+import { toString } from '@ng-bootstrap/ng-bootstrap/util/util';
 
 @Component({
   selector: 'app-progressbar',
   templateUrl: './progressbar.component.html',
   styleUrls: ['./progressbar.component.scss']
 })
-export class ProgressbarComponent {
+export class ProgressbarComponent implements OnInit {
+
+  constructor(
+    @Attribute('label') public label: string,
+    @Attribute('striped') public striped: boolean,
+    @Attribute('thin') public thin: boolean,
+    @Attribute('statusbar') public statusbar: string
+  ){}
+
+  @Input()
+  value: string='0'
+
+
+  ngOnInit(): void {
+  }
 
 }
